4bfd4f36d5bd2785d379becc4861f1b9aed36921
bakal <bakalbondjee86@gmail.com>
2024-02-02 00:22:31 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+/* import { useState } from 'react'
+import './App.css' */
+
+import BMI from './components/BMI';
+import '../src/components/BMI.css'
+
+function App() {
+
+
+  return (
+    <div className='APP'>
+
+      <div className='parent-container'>
+       <BMI />
+      </div>
+    
+    </div>
+  )
+}
+
+export default App
